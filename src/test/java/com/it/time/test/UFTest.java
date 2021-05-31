0a9a1c11bfd917ecde7cd9cf0a3e510366976f22
ljf <979982116@qq.com>
2021-05-31 18:25:57 +0800

    数据结构之并查集

--- src/test/java/com/it/time/test/UFTest.java
+++ src/test/java/com/it/time/test/UFTest.java
@@ -1,9 +1,11 @@
 package com.it.time.test;
 
 import com.it.api.UF;
+import com.it.api.UFTreeWeighted;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -35,5 +37,28 @@ public class UFTest {
             uf.union(p,q);
             System.out.println("总共还有"+uf.count()+"个分组");
         }
+    }
+
+    @Test
+    public void test03(){
+        //创建输入流
+        BufferedReader reader = new BufferedReader(new
+                InputStreamReader(UFTest.class.getClassLoader().getResourceAsStream("traffic_projec t.txt")));
+//读取城市数目，初始化并查集
+        int number = Integer.parseInt(reader.readLine());
+        UFTreeWeighted uf = new UFTreeWeighted(number);
+//读取已经修建好的道路数目
+        int roadNumber = Integer.parseInt(reader.readLine());
+//循环读取已经修建好的道路，并调用union方法
+        for (int i = 0; i < roadNumber; i++) {
+            String line = reader.readLine();
+            int p = Integer.parseInt(line.split(" ")[0]);
+            int q = Integer.parseInt(line.split(" ")[1]);
+            uf.union(p,q);
+        }
+//获取剩余的分组数量
+        int groupNumber = uf.count();
+//计算出还需要修建的道路
+        System.out.println("还需要修建"+(groupNumber-1)+"道路，城市才能相通");
     }
 }
